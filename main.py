0c0c1e9382ae52b444ad08b1753bd2d93bdf0075
Prathickshaw <131335262+Prathickshaw@users.noreply.github.com>
2023-05-08 13:10:37 +0530

    Add files via upload
    My First Commit - pong game

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+from turtle import Turtle
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=680)
+screen.title("pong")
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+screen.listen()
+screen.onkey(go_up,"Up")
+screen.onkey(go_down,"Down")
+
+
+screen.exitonclick()
